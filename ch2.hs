7fc720431c4d6bbee90941fa7a27530594a24b7f
Rand0mW4lk <nuh4u87+randomwalk@gmail.com>
2023-08-05 16:23:39 +0400

    Added exercise numbers in format

--- ch2.hs
+++ ch2.hs
@@ -8,7 +8,9 @@ type Side = Float
 type Radius = Float
 type Vertex = (Float, Float)
 
+{-2.1
 --defining rectangle and rtTriangle in terms of vertex
+-}
 
 Rectangle x y = Polygon [(0,0), (x,0), (x,y), (0,y)]  --[(0,0), (s1,0), (s1,s2), (0,s2)]
 
