a82766d61ba45d54d538afdd6a3a9edaf337b3db
Rand0mW4lk <nuh4u87+randomwalk@gmail.com>
2023-08-05 15:35:57 +0400

    defined rectangle in terms of Polygon

---
+++ ch2.hs
@@ -0,0 +1,14 @@
+data Shape = Rectangle Side Side 
+            | Ellipse Radius Radius
+            | RtTriangle Side Side
+            | Polygon [Vertex]
+    deriving Show
+
+type Side = Float
+type Radius = Float
+type Vertex = (Float, Float)
+
+--defining rectangle and rtTriangle in terms of vertex
+
+Rectangle x y = Polygon[(0,0), (x,0), (x,y), (0,y)]  --[(0,0), (s1,0), (s1,s2), (0,s2)]
+
