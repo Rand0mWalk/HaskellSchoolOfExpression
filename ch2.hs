95fd98ce887a40f340c7ebe9c628b7be57899c46
Rand0mW4lk <nuh4u87+randomwalk@gmail.com>
2023-09-02 15:23:56 +0400

    Heron's formulat equivalence between Polygon with 4 points corresponding to Rectangle area proved

--- ch2.hs
+++ ch2.hs
@@ -28,3 +28,37 @@ regularPolygon n s = Polygon (map get_v [1..n])
                           new_angle i = fromIntegral i*angle
                           r = (s/2)/ (sin half_a)
                           get_v i = (r*cos(new_angle i),r*sin(new_angle i))
+
+{-2.3
+area (Rectangle s1 s2) = area (Polygon [(0,0),(s1,0),(s1,s2),(0,s2)])
+
+Heron's formula :
+A = sqrt(s(s-a)(s-b)(s-c))
+    where s = semiperimeter = (a+b+c)/2
+          a,b,c = lengeth between vertices
+
+starting from RHS
+RHS = triArea (0,0) (s1,0) (s1,s2) + areaPolygon (0,0):(s1,s2):(0,s2)
+    (applying heron's formula on triArea)
+    a = s1, b = s2, c = sqrt(s1^2+s2^2)
+    => sqrt $ s(s-a)(s-b)(s-c)
+    s = (a+b+c)/2 = (s1+s2+c)/2
+    s(s-s1)(s-s2)(s-c) = in two parts
+    s(s-c) = (s1+s2+c)/2 * [(s1+s2+c)/2-c] = 1/4 * (s1+s2 + c) (s1+s2 - c)
+                                           = 1/4 * (s1+s2)^2 - c^2
+                                           = 1/4 * (s1+s2)^2 - (s1^2+s2^2)
+                                           = 1/4 * (s1^2 + 2*s1*s2 + s2^2    - s1^2 - s2^2)
+                                           = 1/4 * 2*s1*s2 = 1/2 * s1* s2
+    (s-s1)(s-s2) = [(s1+s2+c)/2-s1][(s1+s2+c)/2 - s2] = 1/2 * (c+s2-s1) (c-(s2-s1)) =1/4 * (c^2 - (s2-s1)^2)
+                 = 1/4 * (s1^2+s2^2) - [s2^2+s1^2-2*s1*s2] 
+                 = 1/4 * 2*s1*s2 = 1/2 * s1*s2
+
+    therefore sqrt [s(s-c) * (s-s1)(s-s2)] = 1/2 * s1*s2 --this is area formed by first 3 vertices of rectangle
+
+    Similarly - the other remaining triangle (which will recursively compute until wildcard pattern which has 0 area is hit) will also give area of 1/2*s1*s2
+    
+    Combining 1/2*s1*s2 + 1/2*s1*s2 = s1*s2 = area (Rectangle s1 s2)
+
+    As we set out to do. Hence proved. QED
+
+-)
